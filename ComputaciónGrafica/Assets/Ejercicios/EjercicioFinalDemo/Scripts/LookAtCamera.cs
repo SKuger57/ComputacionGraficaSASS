3f8f56551ab6227a3968bfcb1140f375939be0f9
SKuger57 <79534161+SKuger57@users.noreply.github.com>
2023-11-17 00:32:08 -0500

    El efecto mas dificil de mi vida entera

---
+++ ComputaciónGrafica/Assets/Ejercicios/EjercicioFinalDemo/Scripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Camera _cam;
+    private void Start()
+        {
+            _cam = Camera.main;
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = _cam.transform.position - transform.position;
+    }
+}
